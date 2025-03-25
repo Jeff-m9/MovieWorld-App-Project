0151ebcd8a0da4425d9b856629c5fc165dbbe9eb
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-03-25 19:37:26 +0300

    added function

---
+++ src/scripts.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  getMovies();
+});
+
+function getMovies() {
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("http://localhost:3000/movies", options)
+    .then((response) => response.json())
+    .then(renderMovies)
+    .catch((err) => console.error(err));
+}
+
+function renderMovies(movies) {
+  const mainMovieDiv = document.getElementById("main-div");
+
+  mainMovieDiv.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movieContainer = document.createElement("div");
+
+    movieContainer.classList.add("card", "item");
+
+    const movieImage = document.createElement("img");
+    movieImage.classList.add("card-img-top");
+    movieImage.src = movie.image;
+    movieImage.alt = movie.name;
+    movieContainer.appendChild(movieImage);
+
+    const movieContentDiv = document.createElement("div");
+    movieContentDiv.classList.add("card-body");
+
+    const movieName = document.createElement("h2");
+    movieName.classList.add("card-title");
+    movieName.textContent = movie.name;
+    movieContentDiv.appendChild(movieName);
+
+    const movieDescription = document.createElement("p");
+    movieDescription.classList.add("card-text");
+    movieDescription.textContent = movie.description;
+    movieContentDiv.appendChild(movieDescription);
+
+    movieContainer.appendChild(movieContentDiv);
+
+    mainMovieDiv.appendChild(movieContainer);
+  });
+}
